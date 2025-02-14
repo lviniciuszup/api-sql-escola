dc49dffcf3b1c6947487b161ef7c44eca5f4c202
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-14 14:37:35 -0300

    Second commit

---
+++ src/test/java/com/exemplo/escola/EscolaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
