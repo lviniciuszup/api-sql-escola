407e53dd14e0025100ab0c6d31d7d904459db000
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-24 18:25:36 -0300

    Create Alunos model class to represent student data

--- src/main/java/com/exemplo/escola/model/Alunos.java
+++ src/main/java/com/exemplo/escola/model/Alunos.java
@@ -1,4 +1,39 @@
 package com.exemplo.escola.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Alunos {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private Integer idade;
+
+    public Alunos(String nome, Integer idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Alunos() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade){
+        this.idade = idade;
+    }
 }
