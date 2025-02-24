4ed2614ead09b274d9553a442c030f1f26ad72ee
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-24 18:27:18 -0300

    Create AlunosService to encapsulate business logic related to students

--- src/main/java/com/exemplo/escola/services/AlunosService.java
+++ src/main/java/com/exemplo/escola/services/AlunosService.java
@@ -1,4 +1,27 @@
 package com.exemplo.escola.services;
 
+import com.exemplo.escola.model.Alunos;
+import com.exemplo.escola.repository.AlunosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class AlunosService {
+    private final AlunosRepository alunosRepository;
+
+    public AlunosService(AlunosRepository alunosRepository){
+        this.alunosRepository = alunosRepository;
+    }
+    public List<Alunos> listAllAlunos(){
+        return alunosRepository.findAll().stream()
+                .map(alunos -> new Alunos(
+                        alunos.getNome(),
+                        alunos.getIdade()
+                )).collect(Collectors.toList());
+
+
+    }
+
 }
