292b2187584088e4b160d2ae3ac92f58536251c3
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-24 18:24:41 -0300

    Add validation in EscolaApiApplication to check if the database username is null

--- src/main/java/com/exemplo/escola/EscolaApiApplication.java
+++ src/main/java/com/exemplo/escola/EscolaApiApplication.java
@@ -1,5 +1,6 @@
 package com.exemplo.escola;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EscolaApiApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		String dbUsername = dotenv.get("DB_USERNAME");
+		String dbPassword = dotenv.get("DB_PASSWORD");
+
+		if (dbUsername == null || dbPassword == null){
+			throw new IllegalStateException("As variaveis de ambiente precisam ser definidas no arquivo .env");
+		}
+
+		System.setProperty("DB_USERNAME", dbUsername);
+		System.setProperty("DB_PASSWORD", dbPassword);
+
 		SpringApplication.run(EscolaApiApplication.class, args);
 	}
+
 
 }
