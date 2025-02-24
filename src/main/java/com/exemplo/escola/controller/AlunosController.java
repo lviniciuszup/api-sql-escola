da27a88e00c6c091aba2fa078748466d7129baac
lviniciuszup <luiz.vincius@zup.com.br>
2025-02-24 18:27:34 -0300

    Create AlunosController with an endpoint to list students from the database

--- src/main/java/com/exemplo/escola/controller/AlunosController.java
+++ src/main/java/com/exemplo/escola/controller/AlunosController.java
@@ -1,4 +1,24 @@
 package com.exemplo.escola.controller;
 
+
+import com.exemplo.escola.model.Alunos;
+import com.exemplo.escola.services.AlunosService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/alunos")
 public class AlunosController {
+    private final AlunosService alunosService;
+
+    public AlunosController(AlunosService alunosService)
+    {this.alunosService = alunosService;}
+
+    @GetMapping
+    public List<Alunos> listAlunos(){
+        return alunosService.listAllAlunos();
+    }
 }
